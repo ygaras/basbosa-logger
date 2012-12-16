b55d582010b11cfd9129af00e76819f5939dc067
Youssef Garas <ygaras@irisoftonline.com>
2012-12-16 20:42:52 +0200

    Using native console logging functionality to log objects when possible

--- index.js
+++ index.js
@@ -31,7 +31,7 @@
    * showPath (boolean, default true) - specifies whether a path to the file
    * logging the message shoud be output with it level (int, default 3) -
    * specifies the highest log level to be output (***indexing is zero-based***)
-   * poorLogger (boolean, default false) - On browsers without a console object
+   * stringifyObjects (boolean, default false) - Used on browsers without ability to log objects in console
    * uiLogger (boolean, true) - Enable or disable on screen custom logger,
    * usefull on mobile and tablets requires jquery
    * 
@@ -42,7 +42,7 @@
     showTime : true,
     showPath : true,
     level : 2,
-    poorLogger : false,
+    stringifyObjects : false,
     uiLogger : true
   };
 
@@ -101,158 +101,156 @@
    * setOptions method to update logger options after initialization
    * 
    */
-  Logger.prototype.setOptions =
-      function(opts) {
-        // Auto populate logging level
-        typeof Config != 'undefined' && Config.logging != 'undefined'
-            && (defaultOptions.level = Config.logging);
-        if (typeof BasbosaConfig != 'undefined') {
-          if (BasbosaConfig.logging != 'undefined')
-            defaultOptions.level = BasbosaConfig.logging;
+  Logger.prototype.setOptions = function(opts) {
+    // Auto populate logging level
+    typeof Config != 'undefined' && Config.logging != 'undefined'
+        && (defaultOptions.level = Config.logging);
+    if (typeof BasbosaConfig != 'undefined') {
+      if (BasbosaConfig.logging != 'undefined')
+        defaultOptions.level = BasbosaConfig.logging;
 
-          // on iphone, no support for multiple arguments to console.log
-          // function
-          // if (BasbosaConfig.agent.family == 'iPhone') opts.poorLogger = true;
-        }
-        opts = opts || {}; // make this argument optional
-        for ( var optKey in defaultOptions) {
-          this[optKey] =
-              typeof (opts[optKey]) === 'undefined' ? defaultOptions[optKey]
-                  : opts[optKey];
-        }
-        // _.extend(this, defaultOptions, opts);
-      };
+      // on iphone, no support for multiple arguments to console.log
+      // function
+      // if (BasbosaConfig.agent.family == 'iPhone') opts.poorLogger = true;
+    }
+    opts = opts || {}; // make this argument optional
+    for ( var optKey in defaultOptions) {
+      this[optKey] = typeof (opts[optKey]) === 'undefined' ? defaultOptions[optKey] : opts[optKey];
+    }
+    // _.extend(this, defaultOptions, opts);
+  };
 
-  Logger.prototype.initUiLogger =
-      function() {
-        var $buttonMax, $buttonClose;
-        
-        if (typeof $ === 'undefined') return;
-        
-        $(function() {
-          $('body').append($('<div>').addClass('basbosa-logger').css({
-            'position' : 'fixed',
-            'top' : '0',
-            'opacity' : '0.8',
-            'display' : 'block',
-            'width' : '100%',
-            'height' : '100px',
-            'background-color' : 'white',
-            'z-index' : '1001',
-            'overflow' : 'scroll'
-          }));
+  Logger.prototype.initUiLogger = function() {
+    var $buttonMax, $buttonClose;
+    
+    if (typeof $ === 'undefined') return;
+    
+    $(function() {
+      $('body').append($('<div>').addClass('basbosa-logger').css({
+        'position' : 'fixed',
+        'top' : '0',
+        'opacity' : '0.8',
+        'display' : 'block',
+        'width' : '100%',
+        'height' : '100px',
+        'background-color' : 'white',
+        'z-index' : '1001',
+        'overflow' : 'scroll'
+      }));
 
-          $buttonMax =
-              $('<input>').attr({
-                'class' : 'basbosa-logger-max-min',
-                type : 'button',
-                value : '+'
-              }).css({
-                'right' : '45px',
-                'position' : 'fixed'
-              }).click(
-                  function() {
-                    $('.basbosa-logger').css(
-                        'height',
-                        $('.basbosa-logger').css('height') == '100px' ? '100%'
-                            : '100px');
-                  });
-
-          $buttonClose = $('<input>').attr({
-            'claas' : 'basbosa-logger-close',
+      $buttonMax =
+          $('<input>').attr({
+            'class' : 'basbosa-logger-max-min',
             type : 'button',
-            value : 'x'
+            value : '+'
           }).css({
-            'right' : '15px',
+            'right' : '45px',
             'position' : 'fixed'
-          }).click(function() {
-            $('.basbosa-logger').hide();
-          });
+          }).click(
+              function() {
+                $('.basbosa-logger').css(
+                    'height',
+                    $('.basbosa-logger').css('height') == '100px' ? '100%'
+                        : '100px');
+              });
 
-          $('.basbosa-logger').append($buttonMax, $buttonClose);
-        });
+      $buttonClose = $('<input>').attr({
+        'claas' : 'basbosa-logger-close',
+        type : 'button',
+        value : 'x'
+      }).css({
+        'right' : '15px',
+        'position' : 'fixed'
+      }).click(function() {
+        $('.basbosa-logger').hide();
+      });
 
-        isFirstTime = false;
-      };
+      $('.basbosa-logger').append($buttonMax, $buttonClose);
+    });
+
+    isFirstTime = false;
+  };
 
   /**
    * log method.
    * 
    * @api public
    */
-  Logger.prototype.log =
-      function(type) {
-        var index, consoleArgs, filePath = '', lineNumber, traceDetails;
+  Logger.prototype.log = function(type) {
+    var index, consoleArgs, filePath = '', lineNumber, traceDetails;
 
-        for ( var i = 0; i < levels.length; i++) {
-          if (levels[i] == type)
-            index = i;
+    for ( var i = 0; i < levels.length; i++) {
+      if (levels[i] == type)
+        index = i;
+    }
+
+    if (index > this.level || !this.enabled
+        || typeof console == 'undefined'
+        || typeof console.log == 'undefined')
+      return this;
+
+    // Works only for V8 (i.e. Chrome & nodejs)
+    Error.prepareStackTrace = function(error, frames) {
+      var frame, i, basFolder;
+      for (i = 0; i < frames.length; ++i) {
+        if (!frames[i].getFileName())
+          return;
+        // if this is not the path to logging_module.js, assign this frame
+        // to the var 'frame'
+        if (frames[i].getFileName().indexOf('index.js') == -1) {
+          frame = frames[i];
+          break;
         }
+      }
+      filePath = frame.getFileName();
+      // Exclude app root from logged file path
+      if (typeof APP_PATH != 'undefined')
+        filePath = filePath.replace(APP_PATH, '');
+      if (!SERVER)
+        filePath = filePath.replace(window.location.origin, '');
 
-        if (index > this.level || !this.enabled
-            || typeof console == 'undefined'
-            || typeof console.log == 'undefined')
-          return this;
+      lineNumber = frame.getLineNumber();
+      return '';
+    };
 
-        // Works only for V8 (i.e. Chrome & nodejs)
-        Error.prepareStackTrace = function(error, frames) {
-          var frame, i, basFolder;
-          for (i = 0; i < frames.length; ++i) {
-            if (!frames[i].getFileName())
-              return;
-            // if this is not the path to logging_module.js, assign this frame
-            // to the var 'frame'
-            if (frames[i].getFileName().indexOf('index.js') == -1) {
-              frame = frames[i];
-              break;
-            }
-          }
-          filePath = frame.getFileName();
-          // Exclude app root from logged file path
-          if (typeof APP_PATH != 'undefined')
-            filePath = filePath.replace(APP_PATH, '');
-          if (!SERVER)
-            filePath = filePath.replace(window.location.origin, '');
+    try {
+      throw (new Error());
+    }
 
-          lineNumber = frame.getLineNumber();
-          return '';
-        };
-
-        try {
-          throw (new Error());
-        }
-
-        catch (e) {
-          var frames, i;
-          // if in V8, this will change filePath and lineNumber to the expected
-          // values
-          // thanks to Error.prepareStackTrace = ... above
-          e.stack;
-          // if in V8
-          if (!(filePath == '' && lineNumber == -1))
-            traceDetails = '[@' + filePath + ':' + lineNumber + ']';
-          // else (i.e. Firefox)
-          else if (e.stack) {
-            frames = e.stack.split('\n');
-            for (i = 0; i < frames.length; ++i) {
-              // if this is not the path to logging_module.js, assign this frame
-              // to the var 'traceDetails'
-              if (frames[i].indexOf('logging_module') == -1) {
-                traceDetails = frames[i];
-                break;
-              }
-            }
-            basFolder =  window.location.protocol + '//' + window.location.host;
-            traceDetails = '['+ 
-              traceDetails.substring(traceDetails.indexOf('@')).replace(basFolder, '') 
-              + ']';
+    catch (e) {
+      var frames, i;
+      // if in V8, this will change filePath and lineNumber to the expected
+      // values
+      // thanks to Error.prepareStackTrace = ... above
+      e.stack;
+      // if in V8
+      if (!(filePath == '' && lineNumber == -1))
+        traceDetails = '[@' + filePath + ':' + lineNumber + ']';
+      // else (i.e. Firefox)
+      else if (e.stack) {
+        frames = e.stack.split('\n');
+        for (i = 0; i < frames.length; ++i) {
+          // if this is not the path to logging_module.js, assign this frame
+          // to the var 'traceDetails'
+          if (frames[i].indexOf('logging_module') == -1) {
+            traceDetails = frames[i];
+            break;
           }
         }
+        basFolder =  window.location.protocol + '//' + window.location.host;
+        traceDetails = '['+ 
+          traceDetails.substring(traceDetails.indexOf('@')).replace(basFolder, '') 
+          + ']';
+      }
+    }
 
-        consoleArgs = this.getMyConsoleArgs(arguments, this, type, traceDetails, index);
-        console.log && console.log.apply && console.log.apply(console, consoleArgs);
-        return this;
-      };
+    consoleArgs = this.getMyConsoleArgs(arguments, this, type, traceDetails, index);
+    console.log && console.log.apply && console.log.apply(console, consoleArgs);
+    if (this.uiLogger) {
+      this.uiLog(type, consoleArgs);
+    }
+    return this;
+  };
 
   Logger.prototype.getLogTime = function() {
     var date = new Date()
@@ -265,10 +263,13 @@
   };
 
   Logger.prototype.uiLog = function(type, outputConsoleArgs) {
-    if (typeof $ === 'undefined')
-      return;
-    $loggerRow =
-        $('<div>').addClass('basbosa-logger-' + type).html(
+    if (typeof $ === 'undefined') return;
+    for (var i = 0; i < outputConsoleArgs.length; i++) {
+      if (typeof outputConsoleArgs[i] === 'object') {
+        outputConsoleArgs[i] = JSON.stringify(outputConsoleArgs[i]);
+      }
+    }
+    $loggerRow =  $('<div>').addClass('basbosa-logger-' + type).html(
             outputConsoleArgs + '<hr/>');
     $('.basbosa-logger').append($loggerRow);
     $('.basbosa-logger').scrollTop(99999999999);
@@ -279,6 +280,7 @@
     $('.basbosa-logger-debug').css({'color' : '#3333FF'});
     $('.basbosa-logger-trace').css({'color' : '#333366'});
     $('.basbosa-logger-error').css({'color' : 'red'});
+    $('.basbosa-logger-warn').css({'color' : 'red'});
     $('.basbosa-logger-info').css({'color' : '#66CC00'});
   };
 
@@ -290,17 +292,12 @@
     //var inputs = toArray(inputConsoleArgs).slice(1);
 
     for ( var i = 1; i < inputConsoleArgs.length; ++i) {
-      if (inputConsoleArgs[i] == '[object Object]')
+      if (typeof inputConsoleArgs[i] === 'object' && this.stringifyObjects)
         outputConsoleArgs.push(JSON.stringify(inputConsoleArgs[i]));
       else
         outputConsoleArgs.push(inputConsoleArgs[i]);
     }
-
     context.showPath ? outputConsoleArgs.push(traceDetails) : '';
-
-    if (context.uiLogger) {
-      context.uiLog(type, outputConsoleArgs);
-    }
     return outputConsoleArgs;
   };
 
